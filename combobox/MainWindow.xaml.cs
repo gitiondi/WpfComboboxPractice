0c241f33f5c5f560cc81430aac793e8c6c839b24
enrico.biondi <ebiondi@gmail.com>
2022-04-18 09:57:10 +0200

    Initial revision

---
+++ combobox/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace combobox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<string> _property;
+
+        public List<string> ComboItemsList
+        {
+            get => _property;
+            set =>  _property = value;
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _property = new List<string>() { "One", "Two", "Three", "Four", "Five" };
+            DataContext = this;
+        }
+    }
+}
